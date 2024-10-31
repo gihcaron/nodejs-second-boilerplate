09023febd4768052da02bc69a6bce645e76e4cc4
Giovanna Caron <158209996+gihcaron@users.noreply.github.com>
2024-10-31 15:54:24 -0300

    feat: create post user method

--- src/routes/models/users/UsersRepository.js
+++ src/routes/models/users/UsersRepository.js
@@ -1,3 +1,4 @@
+import User from "./User.js";
 class UsersRepository {
     constructor() {
         this.users = [];
@@ -5,6 +6,14 @@ class UsersRepository {
 
     getAllUsers() {
         return this.users;
+    }
+
+    addUser(name, email, password) {
+        const newUser = new User(name, email, password);
+
+        this.users.push(newUser);
+
+        return newUser;
     }
 }
 
